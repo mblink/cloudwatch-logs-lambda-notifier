de6d98ae0f4ef5ce376be97994c4b9119528de26
Matt Dziuban <mrdziuban@gmail.com>
2017-03-23 08:39:18 -0400

    Add eslint, fix errors.

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: 'airbnb/base',
+  rules: {
+    'comma-dangle': ['error', 'never'],
+    'max-len': ['error', 117, 2],
+    'no-confusing-arrow': ['error', { allowParens: true }],
+    'no-console': 'off',
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,37 +1,35 @@
-import {CloudWatchLogs} from 'aws-sdk';
+import { CloudWatchLogs } from 'aws-sdk';
 import sendgrid from 'sendgrid';
 
 const cwLogs = new CloudWatchLogs();
+
+const withObj = (obj, ...fns) => fns.forEach(fn => fn(obj));
 
 const getTimeframe = (message) => {
   const timestamp = new Date(message.StateChangeTime);
   return [new Date(+timestamp - (message.Trigger.Period * message.Trigger.EvaluationPeriods * 1000)), timestamp];
 };
 
-const getMetricFilters = (message) => {
-  return new Promise((resolve, reject) => {
-    const params = {
-      metricName: message.Trigger.MetricName,
-      metricNamespace: message.Trigger.Namespace
-    };
+const getMetricFilters = message => new Promise((resolve, reject) => {
+  const params = {
+    metricName: message.Trigger.MetricName,
+    metricNamespace: message.Trigger.Namespace
+  };
 
-    cwLogs.describeMetricFilters(params, (err, data) => err ? reject(err) : resolve(data));
+  cwLogs.describeMetricFilters(params, (err, data) => (err ? reject(err) : resolve(data)));
+});
+
+const getPaginatedLogs = (params, events) => new Promise((resolve, reject) => {
+  cwLogs.filterLogEvents(params, (err, data) => {
+    if (err) { return reject(err); }
+    const newEvents = events.concat(data.events);
+    return data.nextToken
+      ? getPaginatedLogs(Object.assign(params, { nextToken: data.nextToken }), newEvents).then(resolve)
+      : resolve([params, newEvents]);
   });
-};
+});
 
-const getPaginatedLogs = (params, events) => {
-  return new Promise((resolve, reject) => {
-    cwLogs.filterLogEvents(params, (err, data) => {
-      if (err) { return reject(err); }
-      const newEvents = events.concat(data.events);
-      return data.nextToken
-        ? getPaginatedLogs(Object.assign(params, { nextToken: data.nextToken }), newEvents).then(resolve)
-        : resolve([params, newEvents]);
-    });
-  });
-};
-
-const getLogs = (message, data, nextToken) => {
+const getLogs = (message, data) => {
   if (data.metricFilters.length === 0) { return Promise.reject(new Error('CloudWatch returned no metric filters')); }
 
   const [start, end] = getTimeframe(message);
@@ -46,47 +44,50 @@ const getLogs = (message, data, nextToken) => {
   return getPaginatedLogs(params, []);
 };
 
-const buildEmail = (message, [params, events]) => {
-  return new Promise(resolve => {
-    const from = new sendgrid.mail.Email(process.env.FROM_EMAIL, 'AWS Lambda');
-    const to = new sendgrid.mail.Email(process.env.TO_EMAIL);
-    const subject = `ALARM: "${message.AlarmName}"`;
-    const [start, end] = getTimeframe(message);
-    const logsUrl = 'https://console.aws.amazon.com/cloudwatch/home#logEventViewer' +
-                      `:group=${params.logGroupName};filter=${encodeURIComponent(params.filterPattern)};` +
-                      `start=${start.toISOString()};end=${end.toISOString()}`;
-    const body = new sendgrid.mail.Content('text/html', `
-      <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-      <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
-        <head>
-          <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-          <meta name="viewport" content="width=device-width">
-          <title>${subject}</title>
-        </head>
-        <body>
-          Alarm: ${message.AlarmName}<br>
-          Time: ${end.toString()}<br>
-          Logs:<br><br>
-          <pre>${events.map(e => { try { return JSON.stringify(JSON.parse(e.message), null, 2); } catch (e) { return e.message; } }).join('<hr>')}</pre>
-          <br>
-          View logs:
-          <a href="${logsUrl}">${logsUrl}</a>
-        </body>
-      </html>
-    `);
-
-    resolve(new sendgrid.mail.Mail(from, subject, to, body));
+const buildEmail = (message, [params, events]) => new Promise((resolve) => {
+  const from = new sendgrid.mail.Email(process.env.FROM_EMAIL, 'AWS Lambda');
+  const to = new sendgrid.mail.Email(process.env.TO_EMAIL);
+  const subject = `ALARM: "${message.AlarmName}"`;
+  const [start, end] = getTimeframe(message);
+  const logsUrl = 'https://console.aws.amazon.com/cloudwatch/home#logEventViewer' +
+                    `:group=${params.logGroupName};filter=${encodeURIComponent(params.filterPattern)};` +
+                    `start=${start.toISOString()};end=${end.toISOString()}`;
+  const messages = events.map((e) => {
+    try {
+      return JSON.stringify(JSON.parse(e.message), null, 2);
+    } catch (_) {
+      return e.message;
+    }
   });
-};
+  const body = new sendgrid.mail.Content('text/html', `
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+    <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+      <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta name="viewport" content="width=device-width">
+        <title>${subject}</title>
+      </head>
+      <body>
+        Alarm: ${message.AlarmName}<br>
+        Time: ${end.toString()}<br>
+        Logs:<br><br>
+        <pre>${messages.join('<hr>')}</pre>
+        <br>
+        View logs:
+        <a href="${logsUrl}">${logsUrl}</a>
+      </body>
+    </html>
+  `);
 
-const sendEmail = (email) => {
-  const sg = sendgrid(process.env.SENDGRID_API_KEY);
-  return sg.API(sg.emptyRequest({
-    method: 'POST',
-    path: '/v3/mail/send',
-    body: email.toJSON()
-  }));
-};
+  resolve(new sendgrid.mail.Mail(from, subject, to, body));
+});
+
+const sendEmail = email => withObj(sendgrid(process.env.SENDGRID_API_KEY), sg => sg.API(sg.emptyRequest({
+  method: 'POST',
+  path: '/v3/mail/send',
+  body: email.toJSON()
+})));
 
 exports.handler = (event, context, callback) => {
   const message = JSON.parse(event.Records[0].Sns.Message);
@@ -96,10 +97,10 @@ exports.handler = (event, context, callback) => {
     .then(getLogs.bind(null, message))
     .then(buildEmail.bind(null, message))
     .then(sendEmail)
-    .then(r => {
-      const res = { code: r.statusCode, body: r.body, headers: r.headers };
-      console.log(JSON.stringify({ 'SendGrid response': res }, null, 2));
-      callback(null, res);
-    })
-    .catch(e => { console.error(e); callback(e); });
+    .then(r => withObj(
+      { 'SendGrid response': { code: r.statusCode, body: r.body, headers: r.headers } },
+      res => console.log(JSON.stringify(res, null, 2)),
+      callback.bind(null, null)
+    ))
+    .catch(e => withObj(e, console.error, callback));
 };
