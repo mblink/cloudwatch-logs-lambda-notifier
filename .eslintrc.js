b477a199d4d9f57f361f3f3ae804a61dd4d283c0
Matt Dziuban <mrdziuban@gmail.com>
2017-10-26 15:48:29 -0400

    Update packages.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   env: { mocha: true },
   rules: {
     'comma-dangle': ['error', 'never'],
+    'function-paren-newline': 'off',
     'max-len': ['error', 117, 2],
     'no-confusing-arrow': ['error', { allowParens: true }],
     'no-console': 'off',
